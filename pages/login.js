1e3641f9bf0e11e55576782ae38a9d429f2777a6
Rajitha-PW <rajithachakkilala@gmail.com>
2025-12-23 14:16:48 +0530

    test data json added

--- pages/login.js
+++ pages/login.js
@@ -17,6 +17,7 @@ class Loginpage{
         await this.emailInput.fill(username);
         await this.passwordInput.fill(password);
         await this.submitButton.click();
+        // await this.page.pause();
     }
 }
 
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,24 @@
 import { test, expect } from '@playwright/test';
 import { Loginpage } from '../pages/login';
+import TestData from '../TestData.json';    
 
+test.describe('Login test', () => {
 test('Login feature', async ({page})=>
 {
 
     const loginPage = new Loginpage(page);
     
     await loginPage.navigationTo();
-    await loginPage.login("rajitha007@mailinator.com", "Test@123");
+    await loginPage.login(TestData.validUser.email,TestData.validUser.password);
+    await page.pause();
 
 })
+test('Login with invalid credentials', async ({page})=>{
+    const loginPage = new Loginpage(page);
+    
+    await loginPage.navigationTo();
+    await loginPage.login(TestData.invalidUser.email,TestData.invalidUser.password);
+    await page.pause();
+
+})
+});
